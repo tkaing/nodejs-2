ee3966b139895eddcbade5df94e523c5093443f4
tkaing <t.kaing13@gmail.com>
2019-03-12 11:41:29 +0100

    Init project

---
+++ dates.js
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'test';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+(async function() {
+
+	try {
+		await client.connect();
+		console.log("Connected correctly to server");
+
+		const db = client.db(dbName);
+
+		// Get the collection
+		const col = db.collection('dates');
+
+		// Insert multiple documents
+		const r = await col.insertMany([{ date: (new Date()).toString() }]);
+		assert.equal(1, r.insertedCount);
+
+		// Get documents
+		const docs = await col.find().limit(1).toArray();
+		console.log(docs)
+
+	} catch (err) {
+		console.log(err.stack);
+	}
+
+  	// Close connection
+  	client.close();
+})();
