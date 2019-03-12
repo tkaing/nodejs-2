d4194d4c9adfffa6103fabcf704d14adc3f747a8
tkaing <t.kaing13@gmail.com>
2019-03-12 23:41:24 +0100

    change url

--- chat-bot.js
+++ chat-bot.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 
 // Connection URL + Database Name
-const url = 'mongodb://tkaing:\#12345678@chat-bot-shard-00-00-k7ohr.mongodb.net:27017,chat-bot-shard-00-01-k7ohr.mongodb.net:27017,chat-bot-shard-00-02-k7ohr.mongodb.net:27017/test?ssl=true&replicaSet=chat-bot-shard-0&authSource=admin&retryWrites=true';
+const url = 'mongodb://tkaing:T12345678@chat-bot-shard-00-00-k7ohr.mongodb.net:27017,chat-bot-shard-00-01-k7ohr.mongodb.net:27017,chat-bot-shard-00-02-k7ohr.mongodb.net:27017/test?ssl=true&replicaSet=chat-bot-shard-0&authSource=admin&retryWrites=true';
 const dbName = 'chat-bot';
 
 // GET method route
